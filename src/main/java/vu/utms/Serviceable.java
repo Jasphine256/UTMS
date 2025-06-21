4b14fffadb745346eb1e77359d8f035998ba435c
matovuali <matovumulindwaali@gmail.com>
2025-06-21 14:14:49 +0300

    Update Serviceable.java

--- src/main/java/vu/utms/Serviceable.java
+++ src/main/java/vu/utms/Serviceable.java
@@ -1,6 +1,6 @@
 package vu.utms;
 
-// Serviceable.java - Interface for maintenance
+// Seviceable.java - Interface for maintenance
 public interface Serviceable {
     void performMaintenance();
     boolean isDueForService();
